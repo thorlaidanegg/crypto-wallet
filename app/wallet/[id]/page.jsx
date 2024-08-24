9670b9f5618dfd7e00ff1eaddfadfb33020a3185
RanakMedlr <ranak.shah@medlr.in>
2024-08-24 16:41:36 +0530

    get balance added

---
+++ app/wallet/[id]/page.jsx
@@ -0,0 +1,58 @@
+'use client';
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const Page = ({params}) => {
+  const [balance, setBalance] = useState(0);
+  const { id } = params;
+
+  const getBalance = async (walletId) => {
+    try {
+      const res = await axios.post(
+        'https://solana-mainnet.g.alchemy.com/v2/RXkKhOv8Dzuu2V8gdXF3VlpPl0T1wsVL',
+        {
+          jsonrpc: '2.0',
+          id: 1,
+          method: 'getBalance',
+          params: [walletId],
+        }
+      );
+
+      console.log(res.data.result.value);
+      setBalance(res.data.result.value);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getAccountInfo = async () => {
+    try {
+      const res = await axios.post(
+        'https://solana-mainnet.g.alchemy.com/v2/RXkKhOv8Dzuu2V8gdXF3VlpPl0T1wsVL',
+        {
+          jsonrpc: '2.0',
+          id: 1,
+          method: 'getAccountInfo',
+          params: ['Eg4F6LW8DD3SvFLLigYJBFvRnXSBiLZYYJ3KEePDL95Q'],
+        }
+      );
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+      getBalance(id);
+  }, [id]);
+
+  return (
+    <div>
+      <p>Wallet ID: {id}</p>
+      <p>Account Balance: {balance}</p>
+    </div>
+  );
+};
+
+export default Page;

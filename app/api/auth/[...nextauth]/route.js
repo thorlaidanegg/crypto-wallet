1d61d4f986dda852ec9fd99f52018ca1d7dc5eed
RanakMedlr <ranak.shah@medlr.in>
2024-08-27 22:22:07 +0530

    bug fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,8 @@
-// /app/api/auth/[...nextauth]/route.ts
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
-import connectMongo from '@/lib/dbConnect'; // Ensure this path is correct
-import User from '@/models/User'; // Ensure this path is correct
+import connectMongo from '@/lib/dbConnect';
+import User from '@/models/User';
 
 export const authOptions = {
   providers: [
@@ -18,20 +17,17 @@ export const authOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
+    async signIn({ user, account, profile }) {
       await connectMongo();
 
-      // Check if user already exists
       const existingUser = await User.findOne({ email: user.email });
 
       if (!existingUser) {
-        // Create a new user
         await User.create({
           username: profile.name || user.name,
           email: user.email,
           provider: account.provider,
-          providerId: account.id,
-          // Optional: add any additional default fields, like wallets, etc.
+          providerId: account.providerAccountId,
           wallets: 0,
           solanaWallets: 0,
           ethWallets: 0,
@@ -40,19 +36,15 @@ export const authOptions = {
 
       return true;
     },
-
-    async session({ session, token, user }) {
-      // Pass the user ID or any other data to the session
-      session.userId = user?.id || token?.sub;
+    async session({ session, token }) {
+      session.userId = token.sub;
       session.accessToken = token.accessToken;
       return session;
     },
-
     async jwt({ token, account, user }) {
-      // Store user ID and accessToken in the JWT token
       if (account) {
         token.accessToken = account.accessToken;
-        token.userId = user.id;
+        token.sub = user?.id || token.sub;
       }
       return token;
     },
